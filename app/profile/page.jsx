44d4d6e9296d294d7e2c567cfdb41f9a481c73c7
Ishika <ishika.ighosh@gmail.com>
2024-04-14 00:17:07 +0530

    profile and sevices page added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import Sidebar from '@/components/Sidebar'
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+import { MdPerson } from "react-icons/md";
 function Page() {
   return (
-    <div className='flex flex-row gap-6'>
+    <div className="flex flex-row gap-6">
       <Sidebar />
       {/*Profile page content */}
+      <div className="p-5 flex flex-col gap-8 items-start justify-start w-full mt-16">
+        <h1 className="text-3xl font-bold">Your Profile</h1>
+        <div className="flex flex-row gap-56 w-full justify-center items-center p-6">
+          <div className="flex justify-center items-center border-2 border-blue-300 rounded-xl p-4 md:w-1/4  w-1/2 shadow-xl h-full">
+            <img src="/assets/profile.jpg" alt="" className="w-60 h-60" />
+          </div>
+          <div className="flex flex-col justify-start items-start w-1/2 gap-6 border-2 border-blue-300 shadow-xl rounded-xl p-10 h-full">
+            <h1 className="text-lg font-bold">Username</h1>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="border-2 border-red-600 rounded-lg p-1 w-3/4"
+            />
+            <h1 className="text-lg font-bold">Card no</h1>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="border-2 border-red-600 rounded-lg p-1 w-3/4 "
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,18 +1,55 @@
-import React from 'react'
+import React from "react";
 
 function Services() {
   return (
-    <div className='p-5 flex flex-col gap-4 items-start justify-start w-full mt-16'>
-      <h1 className='text-3xl font-bold'>Our services</h1>
+    <div className="p-5 flex flex-col gap-6 items-start justify-start w-full mt-16">
+      <h1 className="text-3xl font-bold">Our services</h1>
       {/*boxes for services */}
-      <div className='flex flex-wrap gap-10'>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="flex flex-wrap gap-10 w-full">
+        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl border-2 border-blue-300">
+          <img
+            src="/assets/bus.jpg"
+            alt=""
+            className="rounded-xl md:h-52 md:w-60"
+          />
+        </div>
+        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl border-2 border-blue-300">
+          <img
+            src="/assets/auto.jpg"
+            alt=""
+            className="rounded-xl md:h-52 md:w-60"
+          />
+        </div>
+        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl border-2 border-blue-300">
+          <img
+            src="/assets/metro.jpg"
+            alt=""
+            className="rounded-xl md:h-52 md:w-60"
+          />
+        </div>
+        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl border-2 border-blue-300">
+          <img
+            src="/assets/train.jpg"
+            alt=""
+            className="rounded-xl md:h-40 md:w-60"
+          />
+        </div>
+      </div>
+      <div className="flex text-left mt-32">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veniam
+          unde asperiores enim, animi vero, sed eum praesentium quibusdam nobis
+          sequi. Eum omnis at quo ratione provident, explicabo ex. Molestias
+          quos, odit vero quae accusantium inventore, voluptate quam,
+          perspiciatis ex tenetur asperiores minima debitis blanditiis ipsa!
+          Nisi itaque similique labore! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Ullam, modi deleniti, eveniet numquam corporis
+          minima vitae totam voluptas neque excepturi nulla deserunt cumque.
+          Architecto ipsa dolorum error debitis, voluptate quas.
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
