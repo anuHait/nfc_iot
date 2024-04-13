eb7a794c82e3861bc6f5a62db437dd33e47eb365
anuHait <anusmitahait67@gmail.com>
2024-04-14 01:09:20 +0530

    travel page changes

--- app/travel/page.jsx
+++ app/travel/page.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+"use client";
+import React,{useState} from 'react'
 
 function Page() {
+    const [currentStop, setCurrentStop] = useState("A");
+
+
   return (
     <div className='m-20 flex flex-col gap-5 items-center justify-start'>
       <h1 className='text-2xl font-semibold mb-10'>Your Current Travel Status</h1>
 <ol class="items-center sm:flex">
 <li class="relative mb-6 sm:mb-0">
     <div class="flex items-center">
-        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <div class="z-10 flex items-center justify-center w-6 h-6 bg-[#e95b65] rounded-full ring-0 ring-white shrink-0">
+            <svg class="w-2.5 h-2.5 text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
             </svg>
         </div>
@@ -16,12 +20,17 @@ function Page() {
     </div>
     <div class="mt-3 sm:pe-8">
         <time class="block mb-2 text-lg font-semibold leading-nonetext-gray-500">Stop A</time>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+        {
+            currentStop==="A" &&(
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400 w-52">In transit from A to B</p>
+
+            )
+        }
     </div>
 </li>
 <li class="relative mb-6 sm:mb-0">
     <div class="flex items-center">
-        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+        <div class="z-10 flex items-center justify-center w-6 h-6 bg-[#e95b65] rounded-full ring-0 ring-white shrink-0">
             <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
             </svg>
@@ -30,12 +39,12 @@ function Page() {
     </div>
     <div class="mt-3 sm:pe-8">
     <time class="block mb-2 text-lg font-semibold leading-nonetext-gray-500">Stop B</time>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+    <p class="text-base font-normal text-gray-500  w-52">In transit from B to C</p>
     </div>
 </li>
 <li class="relative mb-6 sm:mb-0">
     <div class="flex items-center">
-        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+        <div class="z-10 flex items-center justify-center w-6 h-6 bg-[#e95b65] rounded-full ring-0 ring-white shrink-0">
             <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
             </svg>
@@ -44,12 +53,12 @@ function Page() {
     </div>
     <div class="mt-3 sm:pe-8">
     <time class="block mb-2 text-lg font-semibold leading-nonetext-gray-500">Stop C</time>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+    <p class="text-base font-normal text-gray-500  w-52">In transit from C to D</p>
     </div>
 </li>
 <li class="relative mb-6 sm:mb-0">
     <div class="flex items-center">
-        <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+        <div class="z-10 flex items-center justify-center w-6 h-6 bg-[#e95b65] rounded-full ring-0 ring-white shrink-0">
             <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
             </svg>
@@ -58,7 +67,7 @@ function Page() {
     </div>
     <div class="mt-3 sm:pe-8">
     <time class="block mb-2 text-lg font-semibold leading-nonetext-gray-500">Stop D</time>
-    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+    <p class="text-base font-normal text-gray-500  w-52">Going back to terminus</p>
     </div>
 </li>
 
