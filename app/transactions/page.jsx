57f09a2d103d94ed21b91518212f7caf0e513ae1
Ishika <ishika.ighosh@gmail.com>
2024-04-14 19:36:06 +0530

    new changes

--- app/transactions/page.jsx
+++ app/transactions/page.jsx
@@ -1,38 +1,71 @@
 "use client";
-import React,{useState,useEffect} from 'react'
-import Sidebar from '@/components/Sidebar'
+import React, { useState, useEffect } from "react";
+import Sidebar from "@/components/Sidebar";
+import axios from "axios";
 function Page() {
-  const cardId = localStorage.getItem("cardId");
-  //endpoint = `https://aptcbackendw.vercel.app/get_transaction_history/1a26687e`
+  //const cardId = localStorage.getItem("cardId");
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    async function fetchTransactions() {
+      try {
+        const response = await axios.get(
+          "https://aptcbackendw.vercel.app/get_transaction_history/1a26687e"
+        );
+        setTableData(response.data.transactionHistory);
+      } catch (error) {
+        console.error("Error fetching transaction data:", error);
+      }
+    }
+    fetchTransactions();
+  }, []);
+
   return (
-    <div className='flex flex-row gap-6'>
+    <div className="flex flex-row gap-6">
       <Sidebar />
       <div className="p-5 flex flex-col gap-6 items-start justify-start w-full mt-16">
-      <h1 className="text-3xl font-bold">Your card transactions</h1>
-      <div>
-      <table class="table-fixed w-full  ">
-  <thead>
-    <tr>
-      <th className='py-4 text-lg font-semibold  border-2 border-[#40C0E7]'>Date & Time</th>
-      <th className='py-4 text-lg font-semibold  border-2 border-[#40C0E7]'>From</th>
-      <th className='py-4 text-lg font-semibold  border-2 border-[#40C0E7]'>To</th>
-      <th className='py-4 text-lg font-semibold  border-2 border-[#40C0E7]'>Amount</th>
-    </tr> 
-  </thead>
-  <tbody>
-    <tr>
-      <td className='py-2 bg-gray-100 border-2 border-[#4d84d6] px-3'>12th may </td>
-      <td className='py-2 bg-gray-100 border-2 border-[#4d84d6] px-3'>Malcolm Lockyer</td>
-      <td className='py-2 bg-gray-100 border-2 border-[#4d84d6] px-3'>1961</td>
-      <td className='py-2 bg-gray-100 border-2 border-[#4d84d6] px-3'>300</td>
-    </tr>
-  </tbody>
-</table>
-
-      </div>
+        <h1 className="text-3xl font-bold">Your card transactions</h1>
+        <div>
+          <table class="table-fixed w-full  ">
+            <thead>
+              <tr>
+                <th className="py-4 text-lg font-semibold  border-2 border-[#40C0E7]">
+                  Date & Time
+                </th>
+                <th className="py-4 text-lg font-semibold  border-2 border-[#40C0E7]">
+                  From
+                </th>
+                <th className="py-4 text-lg font-semibold  border-2 border-[#40C0E7]">
+                  To
+                </th>
+                <th className="py-4 text-lg font-semibold  border-2 border-[#40C0E7]">
+                  Amount
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((item) => (
+                <tr>
+                  <td className="py-2 bg-gray-100 border-2 border-[#4d84d6] px-3">
+                    {item.date}
+                  </td>
+                  <td className="py-2 bg-gray-100 border-2 border-[#4d84d6] px-3">
+                    {item.from}
+                  </td>
+                  <td className="py-2 bg-gray-100 border-2 border-[#4d84d6] px-3">
+                    {item.to}
+                  </td>
+                  <td className="py-2 bg-gray-100 border-2 border-[#4d84d6] px-3">
+                    {item.amount}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,46 +1,48 @@
 import React from "react";
+import { SiContactlesspayment } from "react-icons/si";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { MdLockOutline } from "react-icons/md";
+import { MdOutlineAccountBalanceWallet } from "react-icons/md";
+import { BsCreditCard } from "react-icons/bs";
 
 function Services() {
   return (
     <div className="p-5 flex flex-col gap-6 items-start justify-start w-full mt-16">
       <h1 className="text-3xl font-bold">Our card services</h1>
       <div className="flex font-semibold text-lg mt-5 leading-6  text-gray-600 ">
-        <p>
-        Streamline Your Travel Experience with Our Innovative Card System  
-        </p>
+        <p>Streamline Your Travel Experience with Our Innovative Card System</p>
       </div>
       {/*boxes for services */}
-      <div className="flex flex-wrap gap-10 w-full">
-        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl p-2 border-2 border-blue-300">
-          <img
-            src="/assets/bus.jpg"
-            alt="image"
-            className=" w-40 lg:w-44"
-          />
+      <div className="flex flex-wrap gap-10 w-full justify-center">
+        <div className="flex flex-col justify-center items-center shadow-xl w-1/5 rounded-2xl px-6 py-8 gap-4 border-2 border-cyan-400">
+          {/* <img src="/assets/bus.jpg" alt="image" className=" w-40 lg:w-44" /> */}
+          <BsCreditCard className="text-7xl font-bold text-cyan-600" />
+          <h1 className="text-lg font-semibold text-cyan-800 text-center">
+            Seamless Travel Payments
+          </h1>
         </div>
-        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl p-2 border-2 border-blue-300">
-          <img
-            src="/assets/auto.jpg"
-            alt="image"
-            className=" w-40 lg:w-44"
-          />
+        <div className="flex flex-col justify-center items-center shadow-xl w-1/5 rounded-2xl p-4 py-6 gap-4 border-2 border-cyan-400">
+          {/* <img src="/assets/bus.jpg" alt="image" className=" w-40 lg:w-44" /> */}
+          <AiOutlineGlobal className="text-7xl font-bold text-cyan-600" />
+          <h1 className="text-lg font-semibold text-cyan-800">
+            Universal Acceptance
+          </h1>
         </div>
-        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl p-2 border-2 border-blue-300">
-          <img
-            src="/assets/metro.jpg"
-            alt="image"
-            className=" w-40 lg:w-44"
-          />
+        <div className="flex flex-col justify-center items-center shadow-xl w-1/5 rounded-2xl p-4 py-6 gap-4 border-2 border-cyan-400">
+          {/* <img src="/assets/bus.jpg" alt="image" className=" w-40 lg:w-44" /> */}
+          <MdLockOutline className="text-7xl font-bold text-cyan-600" />
+          <h1 className="text-lg font-semibold text-cyan-800">
+            Enhanced Security
+          </h1>
         </div>
-        <div className="flex justify-center items-center shadow-xl w-1/5 rounded-2xl p-2 border-2 border-blue-300">
-          <img
-            src="/assets/train.jpg"
-            alt="image"
-            className=" w-40 lg:w-44"
-          />
+        <div className="flex flex-col justify-center items-center shadow-xl w-1/5 rounded-2xl p-4 py-6 gap-4 border-2 border-cyan-400">
+          {/* <img src="/assets/bus.jpg" alt="image" className=" w-40 lg:w-44" /> */}
+          <MdOutlineAccountBalanceWallet className="text-7xl font-bold text-cyan-600" />
+          <h1 className="text-lg text-center font-semibold text-cyan-800">
+            Convinient Account Management
+          </h1>
         </div>
       </div>
-      
     </div>
   );
 }
